7a1c5a4aa7c402f23a18f907683148c7efccaad0
Bill Gauvey <bgauvey@gmail.com>
2018-12-24 08:27:06 -0500

    removed unused usings

--- slignar-aspnetcore-server/Startup.cs
+++ slignar-aspnetcore-server/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
 using slignaraspnetcoreserver.EF;
 using slignaraspnetcoreserver.Hubs;
